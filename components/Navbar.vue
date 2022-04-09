ded2fb24610c942c4d4488f459e36725dd8e8563
thamudi <tamim.hamoudi@gmail.com>
2022-04-09 15:40:57 +0300

    Added Feature (Header/ Navbar) - Added Header and Navbar - Added some global CSS

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div class="inline-block links">
+      <NuxtLink to="/register" class="nav-link">Join JOSA</NuxtLink>
+      <NuxtLink to="/members" class="nav-link">JOSA Members</NuxtLink>
+      <NuxtLink to="/login" class="nav-link" id="login">Sign In</NuxtLink>
+    </div>
+  </nav>
+</template>
+<style lang="postcss" scoped>
+#login {
+  @apply text-josa-blue border-josa-blue p-2 pt-1 pb-1;
+  border-width: thin;
+}
+
+#login:hover {
+  @apply text-josa-warm-grey-light;
+}
+</style>
