2fc44c10de88b0a26e10fb36322258962551ada0
thamudi <tamim.hamoudi@gmail.com>
2022-04-13 17:48:16 +0300

    Modified Form Component Implemented the 3 phases into the Form component

--- components/register/Form.vue
+++ components/register/Form.vue
@@ -4,26 +4,33 @@
       v-if="step === 1"
       @toggleNext="(formData) => nextStep(formData)"
     />
-    <RegisterPhaseTwo v-else-if="step === 2" />
-    <RegisterPhaseThree v-else-if="step === 3" />
+    <RegisterPhaseTwo
+      v-else-if="step === 2"
+      @toggleNext="(formData) => nextStep(formData)"
+    />
+    <RegisterPhaseThree
+      v-else-if="step === 3"
+      @toggleSubmit="(formData) => submitForm(formData)"
+    />
   </div>
 </template>
 <script setup>
 import { ref, reactive } from 'vue'
 const step = ref(1)
-const formData = reactive({})
+let formData = reactive({})
 const nextStep = (data) => {
-  console.log(data)
+  formData = { ...formData, ...data }
   step.value++
 }
-const previousStep = () => {
-  step.value--
+
+const submitForm = (data) => {
+  formData = { ...formData, ...data }
+  console.log(formData)
 }
 </script>
 <style lang="postcss" scoped>
 .form-container {
   @apply bg-white;
   @apply p-10 my-14;
-  /* @apply max-w-1/2; */
 }
 </style>
