4d6c855fd3b581b93624b7dd6db0d366a7b20456
thamudi <tamim.hamoudi@gmail.com>
2022-04-13 13:39:31 +0300

    Add form CSS Added custom css for forms Modifed nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     // '@/assets/css/typography.css',
     // '@/assets/css/fonts/ibm-plex.css',
     '@/assets/css/layout.css',
+    '@/assets/css/form.css',
     '@/assets/css/global.css'
   ],
   tailwindcss: {
