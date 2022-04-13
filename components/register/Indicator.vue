626329a979ce1ed99b94184e7bc8c2a0d7cd72f8
thamudi <tamim.hamoudi@gmail.com>
2022-04-14 00:12:29 +0300

    Add Form Indicator Added a indicator component to show step type

---
+++ components/register/Indicator.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    :class="
+      props.step == props.phaseNumber ? 'indicator-wrapper' : 'grayed-out'
+    "
+    class="p-8"
+  >
+    <p class="text-wrapper">
+      <span class="phase-number">{{ props.phaseNumber }}.</span>
+      <span>{{ props.phaseText }}</span>
+    </p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  step: {
+    type: Number,
+    default: 1,
+  },
+  phaseNumber: {
+    type: String,
+    default: '1',
+  },
+  phaseText: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<style lang="postcss" scoped>
+.indicator-wrapper {
+  @apply bg-white;
+  @apply h-28;
+}
+.text-wrapper {
+  @apply text-2xl font-bold;
+}
+.phase-number {
+  @apply text-5xl;
+  color: #6db981;
+}
+
+.grayed-out {
+  @apply text-gray-400;
+  @apply opacity-30;
+
+  .phase-number {
+    @apply text-gray-400;
+  }
+}
+</style>
