fcf1243ee3f022e01dfd9d3858d7f97be9be4a55
thamudi <tamim.hamoudi@gmail.com>
2022-04-13 13:39:59 +0300

    Add Form Phase Two

--- components/register/PhaseTwo.vue
+++ components/register/PhaseTwo.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <h3>Phase Two</h3>
+  <div class="input-control">
+    <form @submit.prevent="onSubmit">
+      <label>How can you help JOSA</label>
+      <textarea
+        rows="5"
+        required="required"
+        v-model="form.offerMessage"
+      ></textarea>
+      <label>How JOSA can help you</label>
+      <textarea
+        rows="5"
+        required="required"
+        v-model="form.requestMessage"
+      ></textarea>
+      <div class="text-right">
+        <button class="button-flat button-blue-full" type="submit">
+          Next &rarr;
+        </button>
+      </div>
+    </form>
   </div>
 </template>
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  offerMessage: '',
+  requestMessage: '',
+})
+
+const emit = defineEmits(['toggleNext'])
+const onSubmit = () => {
+  emit('toggleNext', form)
+}
+</script>
+<style lang="postcss" scoped></style>
