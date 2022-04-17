ccc8ad55ad408315d02b55f4c8efe93ef340fa8e
thamudi <tamim.hamoudi@gmail.com>
2022-04-17 15:34:26 +0300

    Fix Hero Banner Fixed design issue with the Banner Filler icon Changed the text in the hero banner to be passed as a prop

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -1,47 +1,65 @@
 <template>
   <div>
     <section class="banner">
-      <div class="content-container container">
-        <div class="content-header">
-          <h1>JOSA Membership Form</h1>
+      <div class="banner-container container">
+        <div class="banner-header">
+          <h1>{{ props.bannerText }}</h1>
         </div>
-        <div class="slash-section">
-          <span>/ / / / / / / / / / / / / / / / / / / / / / / / / / / / /</span>
-        </div>
+        <div class="slash-section"></div>
       </div>
     </section>
   </div>
 </template>
+<script setup>
+const props = defineProps({
+  bannerText: { type: String, default: '' },
+})
+</script>
 <style lang="postcss" scoped>
 * {
   @apply bg-josa-black;
 }
 
 .banner {
-  @apply flex flex-col md:flex-row;
   @apply py-28 mx-9;
   @apply h-auto;
 }
 
-.content-container {
+.banner-container {
+  @apply flex flex-col md:flex-row justify-between;
   @apply text-josa-warm-grey;
-  /* @apply pt-8; */
-  @apply px-8;
-  @apply flex flex-col justify-center md:flex-row md:justify-around;
-  @apply items-baseline;
+  @apply relative;
+  @apply px-20;
 }
 
-.content-header {
+.banner-header {
   /* font-family: 'IBM Mono'; */
-  @apply text-4xl tracking-wide font-normal;
-  /* @apply w-full; */
+  @apply text-3xl lg:text-4xl tracking-wide font-normal;
+  @apply w-full;
 }
-
 .slash-section {
-  /* font-family: 'IBM Sans'; */
-  @apply text-josa-warm-grey-dark;
-  /* @apply w-11/12 lg:w-9/12 w-full; */
-  /* @apply md:mx-3 lg:mx-16; */
-  @apply text-xl lg:text-2xl font-normal;
+  @apply relative;
+  @apply w-full;
+  @apply mt-12 md:mt-0;
+  @apply self-center;
+}
+.slash-section:before {
+  @apply h-8;
+  --border-width: 6px;
+  --stripe-distance: 26px;
+  position: absolute;
+  content: '';
+  left: calc(var(--border-width) * -1);
+  right: calc(var(--border-width) * -1);
+  top: calc(var(--border-width) * -1);
+  bottom: calc(var(--border-width) * -1);
+  background: repeating-linear-gradient(
+    -45deg,
+    #4b5563,
+    transparent 1px,
+    transparent var(--stripe-distance),
+    #4b5563 calc(var(--stripe-distance) + 1px)
+  );
+  z-index: 1;
 }
 </style>
