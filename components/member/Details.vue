fcf7ab8fe952801af1765dfa93643fb75d05ce62
thamudi <tamim.hamoudi@gmail.com>
2022-04-19 11:25:41 +0300

    Fix Profile container Adjusted CSS to match the deisng of the profile container

--- components/member/Details.vue
+++ components/member/Details.vue
@@ -5,41 +5,47 @@
         class="cover"
         :src="props.member.cover ? props.member.cover : placeHolderImages.cover"
       />
-      <div class="flex flex-col lg:flex-row mx-8">
+      <div
+        id="avatar-info-container"
+        class="flex flex-col lg:flex-row mx-8 relative justify-center items-center"
+      >
         <img
           class="avatar"
           :src="
             props.member.avatar ? props.member.avatar : placeHolderImages.avatar
           "
         />
+        <div class="mr-4 w-64 2xl:w-auto 2xl:mr-0"></div>
         <div class="general-info">
-          <p class="member-name">
-            {{ props.member.first_name }} {{ props.member.last_name }}
-          </p>
-          <p class="member-headline">
-            {{
-              props.member.headline
-                ? props.member.headline
-                : 'This is the headline but it can get longer than you can imagine.'
-            }}
-          </p>
-          <p class="member-location">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="18"
-              height="18"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path
-                d="M12 20.9l4.95-4.95a7 7 0 1 0-9.9 0L12 20.9zm0 2.828l-6.364-6.364a9 9 0 1 1 12.728 0L12 23.728zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 2a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"
-                fill="rgba(75,85,99,1)"
-              />
-            </svg>
-            {{
-              props.member.location ? props.member.location : 'Amman, Jordan'
-            }}
-          </p>
+          <div class="flex flex-col items-center lg:items-start">
+            <p class="member-name">
+              {{ props.member.first_name }} {{ props.member.last_name }}
+            </p>
+            <p class="member-headline">
+              {{
+                props.member.headline
+                  ? props.member.headline
+                  : 'This is the headline but it can get longer than you can imagine.'
+              }}
+            </p>
+            <p class="member-location">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="18"
+                height="18"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path
+                  d="M12 20.9l4.95-4.95a7 7 0 1 0-9.9 0L12 20.9zm0 2.828l-6.364-6.364a9 9 0 1 1 12.728 0L12 23.728zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 2a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"
+                  fill="rgba(75,85,99,1)"
+                />
+              </svg>
+              {{
+                props.member.location ? props.member.location : 'Amman, Jordan'
+              }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -125,21 +131,29 @@ img {
   @apply rounded-3xl;
 }
 
+.placeholder {
+  @apply w-64 mr-8;
+}
 .avatar {
-  @apply max-w-1/4;
-  @apply mb-10 ml-0 mr-6;
+  width: 200px;
+  height: 200px;
+  @apply lg:self-start;
+  @apply mb-10;
+  @apply lg:absolute left-0 bottom-0;
 }
 
 .cover {
-  @apply mb-10;
+  @apply -mb-8 lg:mb-4;
 }
 
 .general-info {
+  @apply self-center flex;
   .member-name {
     @apply text-3xl font-semibold;
   }
   .member-headline {
     @apply text-xl;
+    @apply text-center lg:text-left;
     @apply my-2;
   }
 
