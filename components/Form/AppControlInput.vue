75d92f54f1376d07b3dbaae59f063fadc7ead0cf
thamudi <tamim.hamoudi@gmail.com>
2022-04-25 12:09:20 +0300

    Update App Controller Input A placeholder prop has been added

--- components/Form/AppControlInput.vue
+++ components/Form/AppControlInput.vue
@@ -5,6 +5,7 @@
       v-if="props.inputType !== 'textarea'"
       @input="$emit('update:value', $event.target.value)"
       :required="props.isRequired"
+      :placeholder="props.placeholder"
       :type="props.inputType"
     />
     <textarea
@@ -21,6 +22,7 @@ defineEmits(['update:value'])
 const value = ref('')
 const props = defineProps({
   isRequired: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
   inputType: String,
 })
 </script>
