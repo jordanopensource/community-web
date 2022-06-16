c415cdbf6ddd24fc09dd45b69f2ff4c975b67264
thamudi <tamim.hamoudi@gmail.com>
2022-06-16 17:01:55 +0300

    Add members directory indexpage

--- pages/members/index.vue
+++ pages/members/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <UnderConstruction v-if="config.BETA_RELEASE" />
+  <Banner bannerText="JOSA Members" />
+  <section class="container my-16">
+    <h2 class="text-2xl lg:text-4xl mb-8">Directory of JOSA Members</h2>
+    <div class="flex flex-col lg:flex-row-reverse lg:gap-x-8">
+      <div class="shrink-0">
+        <MembersFilter />
+      </div>
+      <div class="grow">
+        <Members />
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
-const config = useRuntimeConfig()
-</script>
+<script setup></script>
