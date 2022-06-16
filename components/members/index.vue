f4d04fea2a0fd0f987436e9aa37d594b31ebe400
thamudi <tamim.hamoudi@gmail.com>
2022-06-16 17:01:01 +0300

    Add member component

---
+++ components/members/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="!pending">
+    <div v-for="(member, index) in members" :key="`item-${index}`">
+      <MembersCard :member="member" />
+    </div>
+  </div>
+</template>
+<script setup>
+const config = useRuntimeConfig()
+let { data: members, pending } = await useFetch(
+  `${config.BASE_API_URL}/member/all`
+)
+</script>
