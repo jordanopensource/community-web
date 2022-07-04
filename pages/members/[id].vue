85865087876e977ba64d13b3e6c26dbc26670890
thamudi <tamim.hamoudi@gmail.com>
2022-07-04 09:35:35 +0300

    Update member page Add member card component

--- pages/members/[id].vue
+++ pages/members/[id].vue
@@ -1,12 +1,21 @@
 <template>
   <Banner bannerText="JOSA Members" />
-  <MemberDetails v-if="!pendingMember" :member="memberData" />
-  <MemberExperience
-    v-if="!pendingEducation && !pendingEducation"
-    :experience="memberExperience"
-    :education="memberEducation"
-  />
-  <MemberContribution />
+  <div class="container">
+    <MemberDetails v-if="!pendingMember" :member="memberData" />
+    <div class="flex gap-x-4">
+      <div class="sm:hidden lg:block">
+        <MemberCard v-if="!pendingMember" :member="memberData" />
+      </div>
+      <div class="flex flex-col w-full">
+        <MemberExperience
+          v-if="!pendingEducation && !pendingEducation"
+          :experience="memberExperience"
+          :education="memberEducation"
+        />
+        <MemberContribution />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 const config = useRuntimeConfig()
