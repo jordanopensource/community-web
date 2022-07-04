aa08ae6572693b4da4dd2dd54c9fb2b7aab105c7
thamudi <tamim.hamoudi@gmail.com>
2022-07-04 07:35:45 +0300

    Add pagination feature

--- components/members/index.vue
+++ components/members/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!pending && state.members.length">
+  <div v-if="state.members?.length">
     <div v-for="(member, index) in state.members" :key="`item-${index}`">
       <MembersCard :member="member" />
     </div>
@@ -7,16 +7,23 @@
   <div v-else>
     <p>No Members found</p>
   </div>
+  <MembersPaginationBar
+    :currentPage="state.page"
+    :totalPages="state.metaData.totalPages"
+    @switchPage="(newPage) => getMembers((state.page = newPage))"
+  />
 </template>
 <script setup>
-import { watch, watchEffect } from 'vue'
+import { watch } from 'vue'
 
 const config = useRuntimeConfig()
 const state = reactive({
   passedName: '',
   isNoneMember: false,
   sortBy: '',
-  members: [],
+  members: {},
+  metaData: {},
+  page: 1,
 })
 
 const props = defineProps({
@@ -34,30 +41,31 @@ const props = defineProps({
   },
 })
 
-const { data: allMembers, pending } = await useFetch(
-  `${config.BASE_API_URL}/member/all`
-)
+const getMembers = async (currentPage) => {
+  fetch(`${config.BASE_API_URL}/member/page/${currentPage}`)
+    .then((response) => response.json())
+    .then((data) => {
+      state.members = Object.create(data.items)
+      state.metaData = Object.create(data.meta)
+    })
+}
 
-// set the value of the state
-watchEffect(() => {
-  if (!state.members.length) {
-    state.members = Object.create(allMembers)
-  }
-})
+const searchMember = async (query) => {
+  fetch(`${config.BASE_API_URL}/member/search?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => (state.members = Object.create(data)))
+}
 
-// filter member by name
+await getMembers(state.page)
+
+// search member by name
 watch(
   () => (state.passedName = props.name),
-  (newValue) => {
+  async (newValue) => {
     if (newValue) {
-      state.members = allMembers.value.filter(
-        (member) =>
-          member.first_name.toLowerCase().indexOf(newValue.toLowerCase()) >
-            -1 ||
-          member.last_name.toLowerCase().indexOf(newValue.toLowerCase()) > -1
-      )
+      await searchMember(newValue)
     } else {
-      state.members = Object.create(allMembers)
+      await getMembers()
     }
   }
 )
@@ -65,36 +73,39 @@ watch(
 // filter none members
 watch(
   () => (state.isNoneMember = props.isChecked),
-  (checked) => {
+  async (checked) => {
     if (checked) {
-      state.members = allMembers.value.filter((member) => !member.josa_id)
+      state.members.items = state.allMembers.items.filter(
+        (member) => !member.josa_id
+      )
     } else {
-      state.members = Object.create(allMembers)
+      await getMembers()
     }
   }
 )
 
+// Order members
 watch(
   () => (state.sortBy = props.sortBy),
   (sortKey) => {
     switch (sortKey) {
       case 'firstName':
-        state.members.sort((a, b) =>
+        state.members.items.sort((a, b) =>
           a.first_name.toLowerCase() > b.first_name.toLowerCase() ? 1 : -1
         )
         break
       case 'lastName':
-        state.members.sort((a, b) =>
+        state.members.items.sort((a, b) =>
           a.last_name.toLowerCase() > b.last_name.toLowerCase() ? 1 : -1
         )
         break
       case 'firstNameRev':
-        state.members.sort((a, b) =>
+        state.members.items.sort((a, b) =>
           a.first_name.toLowerCase() < b.first_name.toLowerCase() ? 1 : -1
         )
         break
       case 'lastNameRev':
-        state.members.sort((a, b) =>
+        state.members.items.sort((a, b) =>
           a.last_name.toLowerCase() < b.last_name.toLowerCase() ? 1 : -1
         )
         break
