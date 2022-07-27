7d94dbfdf285f7863b538234b0c13fa44a12b363
thamudi <tamim.hamoudi@gmail.com>
2022-07-27 13:33:39 +0300

    Add memebr contributions add member directory page add contrib mission components

---
+++ pages/contribute/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Banner bannerText=" Contribute" />
+  <section class="container my-16">
+    <div class="flex flex-col lg:flex-row-reverse lg:gap-x-8">
+      <div class="shrink-0">
+        <MissionFilter
+          @searchMission="(mission) => (state.searchMission = mission)"
+          @filterMissions="(isChecked) => (state.isAssignedMission = isChecked)"
+          @sortMissions="(orderBy) => (state.sortBy = orderBy)"
+        />
+      </div>
+      <div class="grow">
+        <Mission
+          :mission="state.searchMission"
+          :isChecked="state.isAssignedMission"
+          :sortBy="state.sortBy"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  sortBy: '',
+  searchMission: '',
+  isAssignedMission: false,
+})
+</script>
+f
