cefd848d7bb69763a9ab9dab38337f0453d810b2
thamudi <tamim.hamoudi@gmail.com>
2022-09-27 23:41:11 +0300

    refactor: mission info compoenent remove comment

--- components/mission/Info.vue
+++ components/mission/Info.vue
@@ -81,8 +81,8 @@
           <img
             class="memberAvatar"
             :src="
-              props.mission.assigned.avatar_url
-                ? props.mission.assigned.avatar_url
+              props.mission.owner.avatar_url
+                ? props.mission.owner.avatar_url
                 : placeHolderImages.avatar
             "
           />
@@ -93,18 +93,20 @@
           <br /><br />
           <h5>Assigned to</h5>
           <!-- <div class="flex gap-x-4 items-center"> -->
-          <img
-            class="memberAvatar"
-            :src="
-              props.mission.assigned.avatar_url
-                ? props.mission.assigned.avatar_url
-                : placeHolderImages.avatar
-            "
-          />
-          <h3 class="memberName">
-            {{ props.mission.assigned.first_name_en }}
-            {{ props.mission.assigned.last_name_en }}
-          </h3>
+          <div v-if="props.mission.assigned !== null">
+            <img
+              class="memberAvatar"
+              :src="
+                props.mission.assigned.avatar_url
+                  ? props.mission.assigned.avatar_url
+                  : placeHolderImages.avatar
+              "
+            />
+            <h3 class="memberName">
+              {{ props.mission.assigned.first_name_en }}
+              {{ props.mission.assigned.last_name_en }}
+            </h3>
+          </div>
           <!-- </div> -->
         </div>
       </div>
@@ -114,28 +116,6 @@
       <hr />
     </div>
     <div class="spacer"></div>
-    <div class="commentContainer">
-      <div class="commentorAvatarSection">
-        <div class="commentorInfo">
-          <div class="commentor">
-            <h5>
-              George Khoury <br class="hidden" /><span class="commentDate"
-                >Commented 2 days ago</span
-              >
-            </h5>
-          </div>
-          <div class="commentText">
-            <p>
-              JOSA pushes for more transparency in government through the
-              release of open government data, improving access to government
-              information, and releasing data and content under an open
-              government license.
-            </p>
-          </div>
-        </div>
-        <div class="commentorAvatar"></div>
-      </div>
-    </div>
   </div>
 </template>
 <script setup>
@@ -276,72 +256,5 @@ hr {
 .memberName {
   font-weight: bold;
   display: inline-block;
-}
-
-.commentContainer {
-  padding: 0.01em 16px;
-  margin-bottom: 4rem;
-  position: relative;
-  margin-top: 50px;
-
-  @media only screen and (max-width: 550px) {
-    margin-bottom: 12rem;
-  }
-}
-.commentorAvatarSection {
-  width: 188px;
-  height: 188px;
-  background: white;
-  border-radius: 50%;
-}
-.commentorAvatar {
-  width: 60px;
-  height: 60px;
-  border-radius: 15%;
-  background: url('https://avatars.githubusercontent.com/u/329810?v=4') center
-    center;
-  background-size: cover;
-  position: absolute;
-  top: 25%;
-  left: 26px;
-}
-.commentorInfo {
-  position: absolute;
-  top: 0px;
-  left: 110px;
-  width: 70%;
-  height: auto;
-  background: white;
-  border: 1px solid rgba(228, 227, 227, 0.931);
-}
-commentText {
-  margin: 10px;
-  margin-left: 20px;
-  height: 85%;
-  padding: 4px;
-  padding-top: 0px;
-  text-align: left;
-  font-size: 1.11em;
-  border-top-right-radius: 5%;
-  border-bottom-right-radius: 5%;
-}
-
-.commentor {
-  background-color: #c8c8c8;
-  padding: 0.6rem;
-  padding-left: 2rem;
-  font-weight: bold;
-}
-
-.commentDate {
-  color: grey;
-  font-size: 0.9rem;
-  font-weight: lighter;
-}
-
-.hidden {
-  @media screen and (min-width: 744px) {
-    display: none;
-  }
 }
 </style>
