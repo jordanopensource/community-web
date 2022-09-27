73bb9b24c9cbab952fbd4030cc0b03a262b9e5b2
thamudi <tamim.hamoudi@gmail.com>
2022-09-27 11:08:36 +0300

    refactor: implement MissionInfo component in the single mission page

--- pages/contribute/[id].vue
+++ pages/contribute/[id].vue
@@ -1,38 +1,7 @@
 <template>
-  <Banner bannerText="JOSA Members" />
+  <Banner bannerText="Contribute" />
   <div class="container">
-    <MemberBanner v-if="!pendingMember" :member="memberData" />
-    <div class="flex gap-x-4">
-      <div class="hidden lg:block">
-        <MemberCard v-if="!pendingMember" :member="memberData" />
-        <MemberScore />
-      </div>
-      <div class="flex flex-col w-full">
-        <MemberDetails v-if="!pendingMember" :member="memberData" />
-        <MemberExperience
-          v-if="memberExperience.length || memberEducation.length"
-          :experience="memberExperience"
-          :education="memberEducation"
-        />
-        <MemberContribution />
-      </div>
-    </div>
+    <MissionInfo />
   </div>
 </template>
-<script setup>
-const config = useRuntimeConfig()
-const route = useRoute()
-const user_id = route.params.id
-
-const { data: memberData, pending: pendingMember } = await useFetch(
-  `${config.COMMUNITY_API_URL}/member/${user_id}`
-)
-
-const { data: memberExperience, pending: pendingExperience } = await useFetch(
-  `${config.COMMUNITY_API_URL}/experience/${user_id}`
-)
-
-const { data: memberEducation, pending: pendingEducation } = await useFetch(
-  `${config.COMMUNITY_API_URL}/education/${user_id}`
-)
-</script>
+<script setup></script>
