5786e6033f28c02719db0d8d83dc277ce9541cd7
itsmohmans <mohmans@pm.me>
2022-11-03 13:11:20 +0300

    Move member card to the right bar

--- pages/members/[id].vue
+++ pages/members/[id].vue
@@ -4,10 +4,6 @@
     <div class="container p-0">
       <MemberBanner v-if="!pendingMember" :member="memberData.member" />
       <div class="flex gap-x-4">
-        <div class="hidden lg:block">
-          <MemberCard v-if="!pendingMember" :member="memberData.member" />
-          <MemberScore v-if="!pendingMember" :member="memberData.member" />
-        </div>
         <div class="flex flex-col w-full">
           <MemberDetails v-if="!pendingMember" :member="memberData.member" />
           <MemberExperience
@@ -19,6 +15,10 @@
             v-if="!pendingMember && memberData.contributions.length"
             :contributions="memberData.contributions"
           />
+        </div>
+        <div class="hidden lg:block">
+          <MemberCard v-if="!pendingMember" :member="memberData.member" />
+          <MemberScore v-if="!pendingMember" :member="memberData.member" />
         </div>
       </div>
     </div>
@@ -37,7 +37,8 @@ const { data: memberData, pending: pendingMember } = await useFetch(
 <style scoped lang="postcss">
 .details-container {
   @apply bg-white;
-  @apply m-2 lg:my-5;
-  @apply p-4 lg:p-5;
+  @apply m-2 lg:my-4;
+  @apply p-4 lg:p-8;
+  @apply rounded-lg;
 }
 </style>
