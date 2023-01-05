aac66b1f30af8de87b64d42a226854c656ac619b
Mohamed Mansour <mohmans@pm.me>
2023-01-05 14:48:38 +0300

    feat: add login page components

---
+++ components/login/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container input-control">
+    <h2 class="text-xl lg:text-2xl">Sign in to JOSA.community</h2>
+    <div class="divider-slashes"></div>
+    <form>
+      <FormAppControlInput
+        inputType="email"
+        :isRequired="true"
+        v-model:value="form.email"
+      >
+        Email
+      </FormAppControlInput>
+      <FormAppControlInput
+        inputType="password"
+        :isRequired="true"
+      >
+        Password
+      </FormAppControlInput>
+    </form>
+  </div>
+</template>
+<script setup>
+const form = reactive({
+  email: '',
+})
+</script>
+
+<style lang="postcss" scoped>
+.divider-slashes {
+  @apply mt-5 mb-10;
+  @apply h-3;
+}
+div.container {
+  @apply bg-white;
+  @apply mb-5 md:mb-10;
+  @apply p-5 md:p-10;
+  @apply rounded-lg;
+  @apply md:text-lg;
+}
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+<script setup>
+</script>
+<style lang="postcss" scoped>
+</style>
