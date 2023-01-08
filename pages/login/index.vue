32973d8bb24eba4de57662da9cacfa0d41b670a6
Mohamed Mansour <mohmans@pm.me>
2023-01-08 11:25:53 +0300

    feat: add page banner

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="container">
-    <LoginForm
-      v-if="!state.forgotPassword"
-      @forgot-password="(s) => (state.forgotPassword = s)"
-    />
-    <LoginResetPassword
-      v-else
-      @forgot-password="(s) => (state.forgotPassword = s)"
-    />
+  <div>
+    <Banner bannerText="Sign in" />
+    <div class="container">
+      <LoginForm
+        v-if="!state.forgotPassword"
+        @forgot-password="(s) => (state.forgotPassword = s)"
+      />
+      <LoginResetPassword
+        v-else
+        @forgot-password="(s) => (state.forgotPassword = s)"
+      />
+    </div>
   </div>
 </template>
 <script setup>
