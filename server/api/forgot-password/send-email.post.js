c95c928c908c90570e4fd52dc7f26298b00b38ba
Mohamed Mansour <mohmans@pm.me>
2023-02-05 17:57:44 +0300

    fix: make send-email request a POST req instead of GET

--- server/api/forgot-password/send-email.post.js
+++ server/api/forgot-password/send-email.post.js
@@ -2,13 +2,14 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const body = await useBody(event)
   const options = {
-    method: 'GET',
+    method: 'POST',
     headers: {
       "Accept": "*/*",
       "Content-Type": "application/json"
-    }
+    },
+    body: body
   }
-  const url = `${config.public.COMMUNITY_API_URL}/auth/forgot-password/${body.email}`
+  const url = `${config.public.COMMUNITY_API_URL}/auth/forgot-password/`
   const response = await $fetch.raw(url, options);
   return response
 });
