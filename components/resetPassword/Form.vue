2b4547c33da861940daf93e1d8519f0a272e8bc8
Mohamed Mansour <mohmans@pm.me>
2023-02-07 17:12:53 +0300

    feat: add reset password page and component

---
+++ components/resetPassword/Form.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container input-control">
+    <Message
+      v-if="!state.passwordsMatch"
+      title="Passwords do not match" 
+      type="error"
+      class="mb-4"
+    >
+      Please make sure that the password match in both fields.
+    </Message>
+    <form v-if="!state.resetSuccess">
+      <FormAppControlInput
+        v-model:value="form.password_1"
+        input-type="password"
+        :is-required="true"
+        :show-password-icon="true"
+      >
+        New password
+      </FormAppControlInput>
+      <FormAppControlInput
+        v-model:value="form.password_2"
+        input-type="password"
+        :is-required="true"
+        :show-password-icon="true"
+      >
+      Confirm new password
+      </FormAppControlInput>
+      <FormAppButton
+        class="form-submit"
+        btn-style="button-flat button-blue-full"
+        type="submit"
+        @click.prevent="reset"
+      >
+        Reset password
+      </FormAppButton>
+    </form>
+    <div
+      v-if="state.resetSuccess"
+      class="success-container"
+    >
+      <Message title="Success" type="success">
+        Your password has been successfully reset.
+      </Message>
+      <div class="flex flex-row justify-between mt-4">
+        <NuxtLink to="/login">
+          Login
+        </NuxtLink>
+        <NuxtLink to="/">
+          Return to homepage
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const config = useRuntimeConfig()
+
+const form = reactive({
+  password_1: '',
+  password_2: ''
+})
+const state = reactive({
+  passwordsMatch: true,
+  resetSuccess: false
+})
+
+const reset = async () => {
+  state.passwordsMatch = form.password_1 === form.password_2
+  if (state.passwordsMatch) {
+    const token = useRoute().query.token
+    const url = `${config.public.COMMUNITY_API_URL}/auth/reset-password?token=${token}`
+    
+    await $fetch.raw(url, {
+      method: "PATCH",
+      body: JSON.stringify({
+        "password": form.password_1
+      }),
+      onResponse({response}) {
+        state.resetSuccess = response._data.success
+      }
+    })
+  }
+}
+</script>
+<style lang="postcss" scoped>
+div.container {
+  @apply bg-white;
+  @apply my-5 md:my-10;
+  @apply p-5 md:p-10;
+  @apply rounded-lg;
+  @apply md:text-lg;
+  max-width: 480px;
+}
+.form-submit {
+  @apply w-full;
+  @apply rounded-lg;
+}
+.success-container a {
+  @apply text-left;
+  @apply text-community-blue;
+}
+</style>
