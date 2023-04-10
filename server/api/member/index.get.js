aad61aa055cc64a5d7bec7f7c0067b74678b212b
thamudi <tamim.hamoudi@gmail.com>
2023-04-10 12:37:26 +0300

    refactor: add try catch to server/api endpoints

--- server/api/member/index.get.js
+++ server/api/member/index.get.js
@@ -1,18 +1,23 @@
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
-  const config = useRuntimeConfig()
-  const sessionCookie = getCookie(event, `${config.public.SESSION_COOKIE_NAME}`);
+  try {
+    const query = getQuery(event)
+    const config = useRuntimeConfig()
+    const sessionCookie = getCookie(event, `${config.public.SESSION_COOKIE_NAME}`);
 
-  const url = `${config.public.COMMUNITY_API_URL}/member/${query.id}`
-  const options = {
-    method: "GET",
-    headers: {
-      "Accept": "*/*",
-      "Content-Type": "application/json",
-      "Cookie": `${config.public.SESSION_COOKIE_NAME}=` + sessionCookie,
+    const url = `${config.public.COMMUNITY_API_URL}/member/${query.id}`
+    const options = {
+      method: "GET",
+      headers: {
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "Cookie": `${config.public.SESSION_COOKIE_NAME}=` + sessionCookie,
+      }
     }
-  }
 
-  const response = await $fetch.raw(url, options);
-  return response._data
+    const response = await $fetch.raw(url, options);
+    return response._data
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 });
