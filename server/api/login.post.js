4b8e5f2e0ae40c594f3a9860924bdca279f09df0
thamudi <tamim.hamoudi@gmail.com>
2023-04-11 12:55:06 +0300

    Revert "refactor: add try catch to server/api endpoints"
    This reverts commit aad61aa055cc64a5d7bec7f7c0067b74678b212b.

--- server/api/login.post.js
+++ server/api/login.post.js
@@ -1,32 +1,26 @@
 export default defineEventHandler(async (event) => {
-  try {
-    const config = useRuntimeConfig()
-    const body = await readBody(event)
+  const config = useRuntimeConfig()
+  const body = await readBody(event)
 
-    const url = `${config.public.COMMUNITY_API_URL}/auth/`
-    const loginOptions = {
-      method: "POST",
-      body: body,
-      headers: {
-        "Accept": "*/*",
-        "Content-Type": "application/json"
-      }
+  const url = `${config.public.COMMUNITY_API_URL}/auth/`
+  const loginOptions = {
+    method: "POST",
+    body: body,
+    headers: {
+      "Accept": "*/*",
+      "Content-Type": "application/json"
     }
-
-    const loginResponse = await $fetch.raw(url + 'login', loginOptions);
-
-    const authResponse = await $fetch.raw(url, {
-      method: 'GET',
-      headers: {
-        "Accept": "*/*",
-        "Cookie": loginResponse.headers.get('set-cookie')
-      }
-    })
-    appendHeader(event, 'Set-Cookie', loginResponse.headers.get('set-cookie')) // assuming we only set one cookie
-    return authResponse._data
-
-  } catch (error) {
-    console.log(error);
-    return false;
   }
+
+  const loginResponse = await $fetch.raw(url + 'login', loginOptions);
+
+  const authResponse = await $fetch.raw(url, {
+    method: 'GET',
+    headers: {
+      "Accept": "*/*",
+      "Cookie": loginResponse.headers.get('set-cookie')
+    }
+  })
+  appendHeader(event, 'Set-Cookie', loginResponse.headers.get('set-cookie')) // assuming we only set one cookie
+  return authResponse._data
 });
--- server/api/member/index.get.js
+++ server/api/member/index.get.js
@@ -1,23 +1,18 @@
 export default defineEventHandler(async (event) => {
-  try {
-    const query = getQuery(event)
-    const config = useRuntimeConfig()
-    const sessionCookie = getCookie(event, `${config.public.SESSION_COOKIE_NAME}`);
+  const query = getQuery(event)
+  const config = useRuntimeConfig()
+  const sessionCookie = getCookie(event, `${config.public.SESSION_COOKIE_NAME}`);
 
-    const url = `${config.public.COMMUNITY_API_URL}/member/${query.id}`
-    const options = {
-      method: "GET",
-      headers: {
-        "Accept": "*/*",
-        "Content-Type": "application/json",
-        "Cookie": `${config.public.SESSION_COOKIE_NAME}=` + sessionCookie,
-      }
+  const url = `${config.public.COMMUNITY_API_URL}/member/${query.id}`
+  const options = {
+    method: "GET",
+    headers: {
+      "Accept": "*/*",
+      "Content-Type": "application/json",
+      "Cookie": `${config.public.SESSION_COOKIE_NAME}=` + sessionCookie,
     }
-
-    const response = await $fetch.raw(url, options);
-    return response._data
-  } catch (error) {
-    console.log(error);
-    return false;
   }
+
+  const response = await $fetch.raw(url, options);
+  return response._data
 });
