702283008668dcf669f14aedbf38f59bc9252667
thamudi <tamim.hamoudi@gmail.com>
2023-04-19 14:15:49 +0300

    refactor: include logging on members api proxy

--- server/api/member/index.get.js
+++ server/api/member/index.get.js
@@ -2,8 +2,13 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   const config = useRuntimeConfig()
   const sessionCookie = getCookie(event, `${config.public.SESSION_COOKIE_NAME}`);
-
+  console.log(`---------------`);
+  console.log(`Session cookie: `, sessionCookie);
+  console.log(`---------------`);
   const url = `${config.public.communityApiUrl}/member/${query.id}`
+  console.log(`---------------`);
+  console.log(`Full Url`, url);
+  console.log(`---------------`);
   const options = {
     method: "GET",
     headers: {
@@ -13,6 +18,9 @@ export default defineEventHandler(async (event) => {
     }
   }
 
-  const response = await $fetch.raw(url, options);
+  await $fetch.raw(url, options).then(response => { console.log(response._data); }).catch(error => {
+    console.log(error);
+  });
+  const response = await $fetch.raw(url, options)
   return response._data
 });
