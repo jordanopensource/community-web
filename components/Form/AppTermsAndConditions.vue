a85d4bf9d146f889160302e837cb8556cf5f1065
Anas Alhamdan <cis.ash@gmail.com>
2023-08-10 17:28:58 +0300

    add a space before link in accept terms.

--- components/Form/AppTermsAndConditions.vue
+++ components/Form/AppTermsAndConditions.vue
@@ -1,6 +1,6 @@
 <template>
   <FormAppControlInput inputType="checkbox" :isRequired="true">
-    I accept and understand
+    I accept and understand 
     <a
       href="https://www.josa.ngo/info/privacypolicy"
       class="underline text-josa-blue hover:text-josa-black"
