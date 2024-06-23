68b3e792fecb7a1aceb8f51bc16748bd9d1494ca
Mohamed Mansour <mohmans@pm.me>
2024-06-23 17:31:15 +0300

    refactor: remove register middleware

--- composables/state.js
+++ composables/state.js
@@ -5,14 +5,10 @@ export const useAuthenticated = () => useState('auth', () => false)
 // store the user id
 export const userId = () => useState('userId', () => '')
 
-// member data required for user widget
-export const useMember = () => useState('member', () => { })
-
 // check if user is authenticated and update the store accordingly
 export const isAuth = async (cookieName, cookieValue) => {
   const authenticated = useAuthenticated()
   const userID = userId();
-  const member = useMember();
   const config = useRuntimeConfig()
 
   const url = `${config.public.communityApiUrl}/auth/`
@@ -24,8 +20,6 @@ export const isAuth = async (cookieName, cookieValue) => {
     },
     onResponse({ response }) {
       authenticated.value = response.ok
-      const { first_name_en, last_name_en, id, avatar_url, type, josa_member_id } = response._data
-      member.value = { first_name_en, last_name_en, id, avatar_url, type, josa_member_id }
       userID.value = id
     },
     onResponseError({ response }) {
