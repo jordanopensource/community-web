69ee6cb59946515ce1b062e6f2afacebc9fb6ebe
Mohamed Mansour <mohmans@pm.me>
2024-06-27 12:57:58 +0300

    refactor: auth middleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,27 +1,24 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   const { status, getSession, token, data } = useAuth()
-  if (process.client) {
-    const memberId = localStorage.getItem('userId')
-    if (memberId) {
-      userId().value = memberId
-    } else if (token.value) {
-      getSession().then(() => {
-        updateUserId(data.value?.id)
-      })
-    }
+  
+  if (!token.value) return
+  
+  const memberData = useMemberData().value
+  if (
+    memberData &&
+    userId().value === memberData?.member?.id
+  ) return
+  
+  getSession().then(async () => {
+    updateUserId(data.value?.id)
     try {
-      if (!(useMemberData().value || await useFetchMember())) {
-        isAuth().value = false
-        updateUserId(null)
-        updateMember(null)
-      } else {
-        isAuth().value = status.value === 'authenticated'
-      }
-    } catch (error) {
+      updateMember(await useFetchMember(userId().value))
+    } catch(error) {
       console.error(error)
-      isAuth().value = false
     }
-  }
+    isAuth().value = status.value === 'authenticated'
+    console.info("session updated")
+  })
   if (to.name === 'members-settings' && !isAuth().value) {
     return navigateTo(`/`)
   }
