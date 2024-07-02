756c8c4d5002900f7da7048f6c2eb37daa7ee889
George Khoury <g.khoury@josa.ngo>
2024-07-02 12:20:05 +0300

    fix(nuxt.config.ts): update base URL for nuxt-auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -80,7 +80,7 @@ export default defineNuxtConfig({
     },
   },
   auth: {
-    baseURL: 'http://localhost:5000/auth',
+    baseURL: 'https://community.api.prod.josa.ngo/v2/auth',
     provider: {
       type: 'local',
       endpoints: {
