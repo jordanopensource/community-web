4e6155821c1800db0c3423d01f57b843afe8669d
wlaith <laethwazani@gmail.com>
2026-01-13 20:13:22 +0300

    build: upgrade to nuxt v4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from '.nuxt/eslint.config.mjs'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt([eslintPluginPrettierRecommended]).override(
